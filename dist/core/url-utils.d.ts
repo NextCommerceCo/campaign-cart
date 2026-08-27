beab398fe08635b13ad9d5b5723e854e026048bd
Bond <kasemsanm.dev@gmail.com>
2026-08-27 09:35:22 +0700

    Rebuild dist

--- dist/core/url-utils.d.ts
+++ dist/core/url-utils.d.ts
@@ -1,3 +1,4 @@
+export declare const NON_PROPAGATING_PARAMS: Set<string>;
 export declare function preserveQueryParams(targetUrl: string, preserveParams?: string[] | 'all'): string;
 export declare function navigateWithParams(url: string, options?: {
     replace?: boolean;
